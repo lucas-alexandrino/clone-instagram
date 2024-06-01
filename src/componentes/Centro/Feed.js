9e33543715ccf517ab508ffc45d2b8cbef2874a9
Lucas Alexandrino <lucas.alexcontato@gmail.com>
2024-06-01 18:06:42 -0300

    Formatação do Post.js

--- src/componentes/Centro/Feed.js
+++ src/componentes/Centro/Feed.js
@@ -1,15 +1,26 @@
-//import 'Feed.css';
+import './Feed.css';
 
 import Post from './Post/Post.js'
+
 
 export default function Feed() {
     return (
     <div className="Feed">
         <Post 
-        title="Post 1"
+        title="tartinebakery "
+        subtitle="São Paulo, Brazil"
         imgSrc = "https://placehold.co/150x150"
         body="Lorem ipsum"
         />
+
+        <Post 
+        title="tartinebakery "
+        subtitle="São Paulo, Brazil"
+        imgSrc = "https://placehold.co/150x150"
+        body="Lorem ipsum"
+        />  
+
+
 
     </div> )
 }
--- src/componentes/Centro/Post/Post.js
+++ src/componentes/Centro/Post/Post.js
@@ -1,11 +1,17 @@
 import './Post.css'
+import { BsAlignEnd, BsAlignTop, BsThreeDots } from "react-icons/bs";
 
 export default function Post(props) {
     return (
         <div className="Post">
             <div className="card-header">
                 <h3>{props.title}</h3>
+                <BsThreeDots  className="left-icon" />
             </div>
+            <div className="card-header">
+                <h3>{props.subtitle}</h3>
+            </div>
+          
             <div className="card-body">
                 <img src={props.imgSrc} />
                 <p>{props.body}</p>
